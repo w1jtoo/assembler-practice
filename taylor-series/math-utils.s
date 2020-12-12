170a5ec53e8aa276dfcf7ecf4c1f5ad565fab9d7
w1jtoo <wanadooht@gmail.com>
2020-12-12 11:51:42 +0500

    factorial func and tests

--- taylor-series/math-utils.s
+++ taylor-series/math-utils.s
@@ -2,9 +2,29 @@
 .globl fsin
 
 .text
+
+# calculate factorial of positive interger number
+# rdi -- number
+# rax -- result
 fact:
-    mov $1, %eax
+    push    %rcx
+    push    %rdx
+    mov     $1, %rax
+    mov     $1, %rcx
+
+_loop:
+    cmp     %rcx, %rdi
+    jl      _ret
+
+    mul     %rcx
+    inc     %rcx
+
+    jmp     _loop
+_ret:
+    pop     %rdx
+    pop     %rcx
     ret
 
-sin:
+fsin:
+
     ret
--- taylor-series/tests.c
+++ taylor-series/tests.c
@@ -10,12 +10,17 @@ void assert_eq(char *const test_name, int expected, int actual) {
     }
 }
 
-extern int fact(int number);
+extern long int fact(long int number);
 extern float fsin(float number);
 
 int main(void) {
     printf("Running tests...\n");
-    ASSERT_EQ(TRUE, 1, 1);
-    ASSERT_EQ(FALSE, 1, 0);
+    ASSERT_EQ(FactorialOf0, fact(0), 1);
+    ASSERT_EQ(FactorialOf1, fact(1), 1);
+    ASSERT_EQ(FactorialOf2, fact(2), 2);
+    ASSERT_EQ(FactorialOf3, fact(3), 6);
+    ASSERT_EQ(FactorialOf4, fact(4), 24);
+    ASSERT_EQ(FactorialOf5, fact(5), 120);
+    ASSERT_EQ(FactorialOf6, fact(6), 720);
     return 0;
 }
