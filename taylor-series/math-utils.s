8c85213e91ddb71ba9c6695b54fcc60c589772a4
w1jtoo <wanadooht@gmail.com>
2020-12-12 12:37:37 +0500

    fpow and fact refactoring

--- taylor-series/math-utils.s
+++ taylor-series/math-utils.s
@@ -1,30 +1,59 @@
 .globl fact
 .globl fsin
+.globl fpow
 
 .text
 
 # calculate factorial of positive interger number
+#   rdi -> rax
 # rdi -- number
 # rax -- result
 fact:
-    push    %rcx
     push    %rdx
-    mov     $1, %rax
-    mov     $1, %rcx
+    mov     %rdi, %rax              # result = arg1
 
-_loop:
-    cmp     %rcx, %rdi
-    jl      _ret
+    1:
+        dec     %rdi                # arg -= 1
+        jz      2f                  # if arg non zero result *= arg
+        js      3f                  # else result = 1
 
-    mul     %rcx
-    inc     %rcx
+        mul     %rdi
+        inc     %rcx
 
-    jmp     _loop
-_ret:
-    pop     %rdx
-    pop     %rcx
-    ret
+        jmp     1b
+    3:
+        inc     %rax
 
+    2:
+        pop     %rdx
+        ret
+
+# calculate sin of float number using taylor series
+#   xmm0 -> xmm0
+# xmm0 -- number 
+# xmm0 -- result
 fsin:
-
     ret
+
+# calculate positive integer power of float number
+#   xmm0, rdi -> xmm0
+# xmm0 -- base
+# rdi  -- exponent
+.data
+.align 4
+_one:   .long 0
+
+.text
+fpow:
+        finit
+        fld1
+        fsts     _one
+        movss    _one, %xmm1
+    1:
+        dec     %rdi
+        js      2f
+        mulps   %xmm0, %xmm1
+        jmp     1b
+    2:
+        movss   %xmm1, %xmm0
+        ret
--- taylor-series/tests.c
+++ taylor-series/tests.c
@@ -12,9 +12,11 @@ void assert_eq(char *const test_name, int expected, int actual) {
 
 extern long int fact(long int number);
 extern float fsin(float number);
+extern float fpow(float number, int exp);
 
 int main(void) {
     printf("Running tests...\n");
+    printf("Factorial function tests\n");
     ASSERT_EQ(FactorialOf0, fact(0), 1);
     ASSERT_EQ(FactorialOf1, fact(1), 1);
     ASSERT_EQ(FactorialOf2, fact(2), 2);
@@ -22,5 +24,12 @@ int main(void) {
     ASSERT_EQ(FactorialOf4, fact(4), 24);
     ASSERT_EQ(FactorialOf5, fact(5), 120);
     ASSERT_EQ(FactorialOf6, fact(6), 720);
+
+    printf("\nPower function tests\n");
+    // TODO: write right assert float eq macro
+    ASSERT_EQ(ZeroPower, 1, fpow(1.f, 0));
+    ASSERT_EQ(BigExp, 729, fpow(3.f, 6));
+    ASSERT_EQ(fpowEqCMuptiply, 3.14f * 3.14f, fpow(3.14f, 2));
+
     return 0;
 }
